03c2c77f8909efa7307715c89966d3903c65d893
Douglas Alexandrino <douglx9898@gmail.com>
2024-04-04 13:39:11 -0300

    Terceira e ultima aula.

--- PassIn.Api/Controllers/EventsController.cs
+++ PassIn.Api/Controllers/EventsController.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PassIn.Application.UseCases.Events.GetById;
 using PassIn.Application.UseCases.Events.Register;
-using PassIn.Application.UseCases.Events.RegisterAttendee;
 using PassIn.Communication.Requests;
 using PassIn.Communication.Responses;
-using PassIn.Exceptions;
 
 namespace PassIn.Api.Controllers
 {
@@ -39,18 +36,6 @@ namespace PassIn.Api.Controllers
 
 		}
 
-		[HttpPost]
-		[Route("{eventId}/register")]
-		[ProducesResponseType(typeof(ResponseEventJson), StatusCodes.Status201Created)]
-		[ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
-		[ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
-		public IActionResult Register([FromRoute] Guid eventId, [FromBody] RequestRegisterEventJson request)
-		{
-			var useCase = new RegisterAttendeeOnEventUseCase();
-
-			var response = useCase.Execute(eventId, request);
-
-			return Created(string.Empty, response);
-		}
+		
 	}
 }
--- PassIn.Infrastructure/PassInDbContext.cs
+++ PassIn.Infrastructure/PassInDbContext.cs
@@ -12,6 +12,7 @@ namespace PassIn.Infrastructure
 	{
 		public DbSet<Event> Events { get; set; }
 		public DbSet<Attendee> Attendees { get; set; }
+		public DbSet<CheckIn> CheckIns { get; set; }
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
